d20bfd24bcfeaff077fadf9a569012ca82bffb35
Eric <el@DESKTOP-B41TIG1.localdomain>
2019-04-01 16:39:26 -0700

    Adding dependecies

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 import numpy as np
 import tensorflow as tf
 
-from tensorflow import keras
+#from tensorflow import keras
+#I like manual keras from source: 
+import keras
 
-#import os
-#import time
-#import random
-#from datetime import datetime, timedelta
-#from urllib.request import urlopen
+import os
+import time
+import random
+from datetime import datetime, timedelta
+from urllib.request import urlopen
 
 print("done")
 
