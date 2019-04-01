ae8d5c82ae923d292266c2ead985a12b231256a5
Eric <el@DESKTOP-B41TIG1.localdomain>
2019-04-01 16:37:15 -0700

    asdf

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow import keras
+
+#import os
+#import time
+#import random
+#from datetime import datetime, timedelta
+#from urllib.request import urlopen
+
+print("done")
+
